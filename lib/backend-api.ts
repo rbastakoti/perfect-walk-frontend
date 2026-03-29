afbf5e1d4c65bd7f2ce53d202e23c37d308363ea
dhiraj <dhiraj.majhi20@gmail.com>
2026-03-29 15:19:58 +0530

    backend

--- lib/backend-api.ts
+++ lib/backend-api.ts
@@ -5,7 +5,7 @@
 import { auth } from "@/auth";
 
 // Backend API base URL
-const BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL  || 'http://54.197.187.250:8000/' 
+const BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL || 'http://localhost:8000';
 
 /**
  * Make authenticated API call to backend with session validation
